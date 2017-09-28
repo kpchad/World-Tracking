93cc5db62ef9d40284788e42bc9bab4085ebfa61
Kyle Chadwick <kylepchad@gmail.com>
2017-09-27 21:27:34 -0700

    initial commit

---
+++ World Tracking/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  World Tracking
+//
+//  Created by Kyle Chadwick on 9/27/17.
+//  Copyright © 2017 Kyle Chadwick. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
